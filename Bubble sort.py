fac6d9970ba5e65bfe64a923a58892bdc0d28270
pjimenezq <141860508+pjimenezq@users.noreply.github.com>
2023-10-25 23:00:31 -0500

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,12 +1,12 @@
 lista=[4,9,10,1989,13,5,-80,34]
 bandera=False
 while bandera==False:
-    bandera==True
+    bandera=True
     for num in range(len(lista)-1):
         if lista[num]>lista[num+1]:
-            bandra=False
             aux=lista[num]
             lista[num]=lista[num+1]
             lista[num+1]=aux
+            bandera=False
             
 print(lista)
